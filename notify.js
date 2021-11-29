9691b2ade500b606f08a68af9690283f672bb169
yi228 <94777136+yi228@users.noreply.github.com>
2021-11-29 23:03:43 +0900

    Add files via upload

---
+++ notify.js
@@ -0,0 +1,31 @@
+function notifyMain(){
+    var val = confirm("이 페이지를 벗어나면 작성한 내용을 잃게 됩니다. 나가시겠습니까?")
+    if(val){
+        location.replace('main.html');
+    }
+}
+
+function notifyIntro(){
+    var val = confirm("이 페이지를 벗어나면 작성한 내용을 잃게 됩니다. 나가시겠습니까?")
+    if(val){
+        location.replace('intro.html');
+    }
+}
+function notifyService(){
+    var val = confirm("이 페이지를 벗어나면 작성한 내용을 잃게 됩니다. 나가시겠습니까?")
+    if(val){
+        location.replace('service_info.html');
+    }
+}
+function notifyQna(){
+    var val = confirm("이 페이지를 벗어나면 작성한 내용을 잃게 됩니다. 나가시겠습니까?")
+    if(val){
+        location.replace('qna.html');
+    }
+}
+function notifyNotice(){
+    var val = confirm("이 페이지를 벗어나면 작성한 내용을 잃게 됩니다. 나가시겠습니까?")
+    if(val){
+        location.replace('notice.html');
+    }
+}
